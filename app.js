e2356b1550cdf19241a8c70e9f0feb6486fc7aae
Widz <widz@gmail.com>
2023-10-17 12:48:36 -0800

    Remove comment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Puedes cambiar el puerto si lo deseas
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
